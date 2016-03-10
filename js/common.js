323fd4a0620f2240b735d3551f79d76e1fdb9b47
CrawlingDragon <363630867@qq.com>
2016-03-10 17:09:05 +0800

    2016--3-10
    2016--3-10

---
+++ js/common.js
@@ -0,0 +1,109 @@
+$(document).ready(function(){
+
+
+		$('.message-title').hover(function(){
+			$('.messageBox').show();
+			$('.header-left').css({"background":"#fff","border":"1px solid #e5e5e5","border-bottom":"none","border-top":"none"});
+			$(this).addClass('active');
+		},function(){
+			$('.messageBox').hide();
+			$('.header-left').css({"background":"#f5f8fa","border":"none"});
+			$(this).removeClass('active');
+		})
+		$('.messageBox').hover(function(){
+			$(this).show();
+			$('.header-left').css("background","#fff");
+		},function(){
+			$(this).hide();
+			$('.header-left').css("background","#f5f8fa")
+		});
+
+		//账户显示
+		$('.list1').hover(function(){
+			$(this).find('.list1-cont').show();
+			$(this).addClass('active');
+			$(this).find('.list-title').addClass('active');
+		},function(){
+			$(this).find('.list1-cont').hide();
+			$(this).find('.list-title').removeClass('active');
+			$(this).removeClass('active');
+		})
+		//我的订单
+		$('.list2').hover(function(){
+			$(this).find('.list1-cont').show();
+			$(this).addClass('active');
+			$(this).find('.list-title').addClass('active');
+		},function(){
+			$(this).find('.list1-cont').hide();
+			$(this).find('.list-title').removeClass('active');
+			$(this).removeClass('active');
+		})
+
+		//收藏夹
+		$('.list3').hover(function(){
+			$(this).find('.list1-cont').show();
+			$(this).addClass('active');
+			$(this).find('.list-title').addClass('active');
+		},function(){
+			$(this).find('.list1-cont').hide();
+			$(this).find('.list-title').removeClass('active');
+			$(this).removeClass('active');
+		})
+		//卖家中心
+		$('.list4').hover(function(){
+			$(this).find('.list1-cont').show();
+			$(this).addClass('active');
+			$(this).find('.list-title').addClass('active');
+		},function(){
+			$(this).find('.list1-cont').hide();
+			$(this).find('.list-title').removeClass('active');
+			$(this).removeClass('active');
+		})
+		//搜索框的下拉菜单显示
+        $('.search-list').hover(function(){
+        	$(this).find('ul').addClass('active');
+        },function(){
+        	$(this).find('ul').removeClass('active')
+        })
+		//搜索框的下拉菜单点击选择
+        $('.serachBar li').click(function(){
+        	var thisTxt = $(this).text();
+        	var activeTxt = $(this).parent().find('.selected').html(thisTxt);
+        	$(this).parent().removeClass('active');
+        })
+
+
+        //区域商铺显示
+		 $(".areaShops .shopTitle").click(function(){
+			  $(".areaShops .shopContent").fadeToggle(500);
+		});
+
+
+
+
+
+
+
+
+
+
+
+
+	})
+
+
+
+//收藏夹功能
+		function AddFavorite(title,url){
+			try{
+			   window.external.addFavorite(url,title);
+			 }
+			catch(e){
+			 try{
+			    window.sidebar.addPanel(title,url,"");
+			  }
+			 catch(e){
+			   alert("抱歉，您所使用的浏览器无法完成此操作。\n\n请使用快捷键Ctrl+D进行添加！");
+			   }
+			 }
+		}
--- js/scroll.js
+++ js/scroll.js
@@ -5,48 +5,52 @@
 	window.onscroll =function(){
 		var osTop = document.documentElement.scrollTop || document.body.scrollTop;
 		console.log(osTop);
-		if(osTop >= 0 && osTop<1119){
-			myBar.style.top ='2px';
+		if(osTop<=800){
+			$('#leftbar').hide();
+		}
+		else if(osTop >= 800 && osTop<1119){
+			$('#leftbar').show();
 			$("#a1").find(".z1").hide();
 			$("#a1").find(".z2").show().css("border","");
 			$("#a1").siblings().find(".z2").hide();
 			$("#a1").siblings().find(".z1").show();
 			$("#a1").prev().find(".z1").css("border-bottom","none");
 		}else if(osTop >= 1200 && osTop<1799){
-			myBar.style.top ='562px';
+			$('#leftbar').show();
 			$("#a2").find(".z1").hide();
 			$("#a2").find(".z2").show().css("border","");
 			$("#a2").siblings().find(".z2").hide();
 			$("#a2").siblings().find(".z1").show();
 			$("#a2").prev().find(".z1").css("border-bottom","none");
 		}else if(osTop >=1800 && osTop<2200){
-			myBar.style.top ='1116px';
+			$('#leftbar').show();
 			$("#a3").find(".z1").hide();
 			$("#a3").find(".z2").show().css("border","");
 			$("#a3").siblings().find(".z2").hide();
 			$("#a3").siblings().find(".z1").show();
 			$("#a3").prev().find(".z1").css("border-bottom","none");
 		}else if(osTop >=2201 && osTop<2699){
-			myBar.style.top ='1533px';
+			$('#leftbar').show();
 			$("#a4").find(".z1").hide();
 			$("#a4").find(".z2").show().css("border","");
 			$("#a4").siblings().find(".z2").hide();
 			$("#a4").siblings().find(".z1").show();
 			$("#a4").prev().find(".z1").css("border-bottom","none");
 		}else if(osTop >=2700 && osTop<3216){
-			myBar.style.top ='2085px';
+			$('#leftbar').show();
 			$("#a5").find(".z1").hide();
 			$("#a5").find(".z2").show().css("border","");
 			$("#a5").siblings().find(".z2").hide();
 			$("#a5").siblings().find(".z1").show();
 			$("#a5").prev().find(".z1").css("border-bottom","none");
 		}else if(osTop >=3216){
-			myBar.style.top ='2271px';
+			
 			$("#a5").find(".z1").hide();
 			$("#a5").find(".z2").show().css("border","");
 			$("#a5").siblings().find(".z2").hide();
 			$("#a5").siblings().find(".z1").show();
 			$("#a5").prev().find(".z1").css("border-bottom","none");
+			$('#leftbar').hide();
 		}
 	}
 }
